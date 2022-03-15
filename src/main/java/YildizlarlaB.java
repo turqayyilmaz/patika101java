095f2d2187de929b99246696a66fd42a50309ddf
turqayyilmaz <turqayyilmaz@hotmail.com>
2022-03-15 12:25:03 +0300

    Yıldızlarla B harfi yazdırma

--- src/main/java/YildizlarlaB.java
+++ src/main/java/YildizlarlaB.java
@@ -1,10 +1,9 @@
 public class YildizlarlaB {
     public static void main(String[] args) {
-        String[][] letter = new String[6][4];
-
+        String[][] letter = new String[7][4];
         for (int i = 0; i < letter.length; i++) {
             for (int j = 0; j < letter[i].length; j++) {
-                if (i == 0 || i == 2 || i == 5) {
+                if (i == 0 || i == 3 || i == 6) {
                     letter[i][j] = " * ";
                 } else if (j == 0 || j == 3) {
                     letter[i][j] = " * ";
