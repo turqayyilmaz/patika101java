f567a02625d47090c665a2c15ec07aa635a727a2
turqayyilmaz <turqayyilmaz@hotmail.com>
2022-03-16 09:18:23 +0300

    Dizi Elemanlarını Küçükten Büyüğe Sıralama

---
+++ src/main/java/DiziElemanFrekans.java
@@ -0,0 +1,29 @@
+public class DiziElemanFrekans {
+
+    static int isFind(int[] arr, int val) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == val) return i;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{10, 20, 20, 10, 10, 20, 5, 20};
+        int[] tempArr = new int[arr.length];
+        int tempIndex = -1;
+        for (int i = 0; i < arr.length; i++) {
+            int count = 0;
+            int index = isFind(tempArr, arr[i]);
+            if (index >= 0) {
+                continue;
+            }
+            tempArr[++tempIndex] = arr[i];
+            for (int j = 0; j < arr.length; j++) {
+                if (arr[i] == arr[j]) {
+                    count++;
+                }
+            }
+            System.out.println(arr[i] + " sayısı " + count + " kere tekrar edildi.");
+        }
+    }
+}
---
+++ src/main/java/DiziSiralama.java
@@ -0,0 +1,44 @@
+import java.sql.SQLOutput;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class DiziSiralama {
+    /*
+    Dizinin boyutu n : 5
+Dizinin elemanlarını giriniz :
+1. Elemanı : 99
+2. Elemanı : -2
+3. Elemanı : -2121
+4. Elemanı : 1
+5. Elemanı : 0
+Sıralama : -2121 -2 0 1 99
+     */
+
+    public static void main(String[] args){
+        Scanner scan= new Scanner(System.in);
+
+        System.out.print("Dizinin Boyutu: ");
+        int n = scan.nextInt();
+        int[] arr=new int[n];
+        System.out.println("Dizinin elemanlarını giriniz:");
+
+
+        for(int i =1; i<=n;i++)
+        {
+            System.out.print(i + ". Elemanı: ");
+            arr[i-1]=scan.nextInt();
+        }
+        for(int i=0;i<arr.length;i++){
+            int min=arr[i];
+            for(int j=i+1;j<arr.length;j++){
+                if(min>arr[j]){
+                    int temp= min;
+                    arr[i]=arr[j];
+                    arr[j]=min;
+                    min=arr[i];
+                }
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
